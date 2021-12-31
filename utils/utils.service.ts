010072bcbf2eab9e31f8e9833309c40da74750b7
nclbiagio <ncl.biagio@gmail.com>
2021-12-31 10:26:47 +0100

    test lint staged

--- utils/utils.service.ts
+++ utils/utils.service.ts
@@ -1,6 +1,6 @@
-import { MockApiSchema, MockApiServiceSchema, DefaultMockRequestConfig } from '../app/api-mock-schema.model';
 
-export const getMockPath = (projectName: string, feature: string, serviceId: string) => {
+export const getMockPath = (projectName: string, f
+   eature: string, serviceId: string) => {
    if (feature) {
       return `projects/${projectName}/mock/${feature}/${serviceId}.json`;
    }
